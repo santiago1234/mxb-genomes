b1e479861a9b8c4c3e4b9cfe11867359cbf51eaf
Santiago Medina <santiagogmm@hotmail.com>
2021-04-08 15:06:26 -0500

    Local Ancestry data-Viz

--- analysis-doc/210322-LocalAncestryVisuzlization/karyotype-local-ancestry-plot.R
+++ analysis-doc/210322-LocalAncestryVisuzlization/karyotype-local-ancestry-plot.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 library(scales)
+library(ggthemes)
 
+theme_set(theme_tufte(base_family = "Helvetica"))
 bed <- read_tsv("data/bed/allchrn-HG01893.tsv")
-
+individual <- "HG01893"
 
 # aling chromosomes so the 1st position is zero ---------------------------
 
@@ -44,32 +46,38 @@ bed <- bed %>%
 
 bed %>% 
   ggplot(aes(x = spos, fill = assignment)) +
-  geom_rect(aes(xmin = spos, xmax=epos, ymin = ymin_haplo, ymax = ymax_haplo)) +
-  geom_hline(yintercept = 0.5, color ="white", size = 1) +
-  facet_grid(chm ~., switch = "y") +
-  scale_x_continuous(labels = label_number(scale = 1e-6, suffix = "Mb")) +
+  geom_rect(
+    aes(xmin = spos, xmax=epos, ymin = ymin_haplo, ymax = ymax_haplo)
+  ) +
+  geom_hline(yintercept = 0.5, color ="white", size = 0.1) +
+  facet_grid(chm ~.,switch = "y") +
+  scale_x_continuous(
+    expand = c(0,0),
+    labels = label_number(scale = 1e-6, suffix = "Mb")
+  ) +
   theme(
     axis.ticks.y = element_blank(),
     axis.text.y = element_blank(),
+    strip.text.y = element_text(angle = 0),
     panel.background = element_blank(),
-    legend.position = c(.8, .4)
+    legend.position = c(.8, .2)
   ) +
   labs(
-    y = NULL
+    y = "Chromosome"
   ) +
   scale_fill_viridis_d(option = "C") +
   labs(
-    x = "Position relative to chromosome start"
+    x = "Position relative to chromosome start",
+    title = individual
   )
-ggsave("test.pdf", height = 2, width = 8)
-ggsave("test.png", height = 2, width = 8)
+ggsave("test.pdf", height = 5, width = 5)
+ggsave("test.png", height = 5, width = 5)
 
 
 # this function is to visualy check that points are placed correct --------
 
 
 bed %>% 
-  #filter(chm == 22) %>% 
   ggplot(aes(x = spos, fill = assignment)) +
   geom_point(aes(y = Haplotype),shape = 21, position = "jitter") +
   geom_hline(yintercept = 0.5, color ="white", size = 1) +
@@ -86,6 +94,7 @@ bed %>%
   ) +
   scale_fill_viridis_d(option = "C") +
   labs(
-    x = "Position relative to chromosome start"
+    x = "Position relative to chromosome start",
+    subtitle = individual
   )
 ggsave("test-points.pdf", height = 3, width = 8)
