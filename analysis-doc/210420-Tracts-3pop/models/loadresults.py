840e8db7e6a0ef74ec00415ac4b432159d2f0589
Santiago Medina <santiagogmm@hotmail.com>
2021-04-26 23:40:44 -0500

    compute bootraps intervals

--- analysis-doc/210420-Tracts-3pop/models/loadresults.py
+++ analysis-doc/210420-Tracts-3pop/models/loadresults.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import glob
 import numpy as np
+import scipy.stats as st
 
 
 def _list_boostrap_files(dir_to_res, boot_n=0):
@@ -42,15 +43,45 @@ def load_boot_rest(dir_to_res, boot_n, labels):
         "helper function to construc data frame"
         df = pd.DataFrame(arr_d.T, columns=labels)
         df['bins'] = bins
-        df = df.melt(id_vars=['bins'], var_name="Ancestry", value_name=var_name)
+        df = df.melt(id_vars=['bins'],
+                     var_name="Ancestry", value_name=var_name)
         return df
 
     dat = make_frame(dat, 'dat')
     pred = make_frame(pred, 'pred')
-    return pd.merge(dat, pred, how='inner', on=['bins', 'Ancestry'])
+    data = pd.merge(dat, pred, how='inner', on=['bins', 'Ancestry'])
+    data['boot'] = boot_n
+    return data
 
 
+def confIntMean(a, conf=0.95):
+    mean, sem, m = np.mean(a), st.sem(a), st.t.ppf((1+conf)/2., len(a)-1)
+    return mean, mean - m*sem, mean + m*sem
+
+############
+# TODO: put this into a function
 dir_to_res = "output/"
 labels = ['NAT', 'EUR', 'AFR']
 datos = load_boot_rest(dir_to_res, 0, labels)
+
+# compute mean and confidence interval
+
+boots = list(range(1, 20))
+boots = pd.concat([load_boot_rest(dir_to_res, x, labels) for x in boots])
+
+# Compute the cofiden interval for preds
+#
+res = (
+    boots.
+    groupby(['bins', 'Ancestry'])['pred'].
+    apply(confIntMean).
+    reset_index()
+)
+
+res['mean'] = res.pred.map(lambda x: x[0])
+res['ci_u'] = res.pred.map(lambda x: x[1])
+res['ci_l'] = res.pred.map(lambda x: x[2])
+res.drop(['pred'], axis=1, inplace=True)
+datos = datos.merge(res, how='inner', on=['bins', 'Ancestry'])
 datos.to_csv("mxl.csv", index=False)
+
