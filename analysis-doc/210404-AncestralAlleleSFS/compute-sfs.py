53bd1c248fc617fde43cb79ab4e30cfe3d160cab
santiago1234 <santiagogmm@hotmail.com>
2021-05-11 11:35:47 -0500

    snakefile to compute SFS

--- analysis-doc/210404-AncestralAlleleSFS/compute-sfs.py
+++ analysis-doc/210404-AncestralAlleleSFS/compute-sfs.py
@@ -1,5 +1,29 @@
+"""
+Compute unfolded SFS from VCF file:
+usage:
+	python <VCF> <AA> <output>
+Note:
+	This script migth only work inside this dir
+	due to relative paths used.
+"""
 import sys
-sys.path.append("../../mxbgenomes")
+sys.path.append("../..")
 from mxbgenomes import sfs
+from mxbgenomes.utils import load_populations_info
 
+vcf_file = sys.argv[1]
+aa_file = sys.argv[2]
+outfile = sys.argv[3]
 
+popinfo = load_populations_info("../../")
+
+subpops = (
+    popinfo
+    .groupby('Subpopulation')
+    .apply(lambda x: x.Samplename.to_list())
+    .to_dict()
+)
+
+spectrum = sfs.sfs_unfolded(vcf_file, aa_file, subpops=subpops, project_haplod_size=50)
+spectrum = sfs.sfs_to_frame(spectrum)
+spectrum.to_csv(outfile, index=False)
