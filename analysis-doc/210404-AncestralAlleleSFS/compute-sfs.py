3ba49353ba751649dcb21177e584230fd77de7b9
Santiago Medina <santiagogmm@hotmail.com>
2021-05-12 12:50:21 -0500

    save aa stats output

--- analysis-doc/210404-AncestralAlleleSFS/compute-sfs.py
+++ analysis-doc/210404-AncestralAlleleSFS/compute-sfs.py
@@ -1,7 +1,7 @@
 """
 Compute unfolded SFS from VCF file:
 usage:
-	python <VCF> <AA> <output>
+	python <VCF> <AA> <output> <statsfile>
 Note:
 	This script migth only work inside this dir
 	due to relative paths used.
@@ -14,6 +14,7 @@ from mxbgenomes.utils import load_populations_info
 vcf_file = sys.argv[1]
 aa_file = sys.argv[2]
 outfile = sys.argv[3]
+statsfile sys.argv[4]
 
 popinfo = load_populations_info("../../")
 
@@ -24,6 +25,7 @@ subpops = (
     .to_dict()
 )
 
-spectrum = sfs.sfs_unfolded(vcf_file, aa_file, subpops=subpops, project_haplod_size=50)
-spectrum = sfs.sfs_to_frame(spectrum)
+spectrum, stats = sfs.sfs_unfolded(vcf_file, aa_file, subpops=subpops, project_haplod_size=50)
+spectrum, stats = sfs.sfs_to_frame(spectrum)
 spectrum.to_csv(outfile, index=False)
+stats.to_csv(statsfile, index=False)
