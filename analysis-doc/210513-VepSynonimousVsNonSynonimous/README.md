520952fa5de42a87a9d235b9c46c3a81733d2e91
Santiago Medina <santiagogmm@hotmail.com>
2021-05-19 10:32:10 -0500

    results in readme file

--- analysis-doc/210513-VepSynonimousVsNonSynonimous/README.md
+++ analysis-doc/210513-VepSynonimousVsNonSynonimous/README.md
@@ -11,4 +11,32 @@ I will look at the SFS distribution for the variants in the following categories
     - Missense variantlll
 
 For each of these, I will look at the SFS distribution, for each populations. This is similar to the
-`[previous sfs analysis](../210404-AncestralAlleleSFS).
+[previous sfs analysis](../210404-AncestralAlleleSFS).
+
+
+# Results
+
+**SFS stats**
+
+This table is for synonymous variants.
+
+| case              |      n |         p |
+|:------------------|-------:|----------:|
+| aa_is_ref         | 127007 | 90.6927   |
+| aa_is_alt         |   9228 |  6.5895   |
+| aa_is_dot         |   2499 |  1.78448  |
+| aa_is_N           |    141 |  0.100685 |
+| aa_missing        |    619 |  0.442013 |
+| aa_not_ref_or_alt |    547 |  0.3906   |
+
+
+![sfs](plots/sfs.png)
+
+
+Focus on low frequency and high frequency variants.
+
+![sfs_low_high](plots/sfs-maf.png)
+
+Comparing the number of Non-synonymous variants to synonymous.
+
+![syntonon](plots/NtoNS.png)
--- analysis-doc/210513-VepSynonimousVsNonSynonimous/plot.R
+++ analysis-doc/210513-VepSynonimousVsNonSynonimous/plot.R
@@ -1,8 +1,6 @@
 library(tidyverse)
-library(gghighlight)
-library(ggthemes)
-library(gridExtra)
-theme_set(theme_tufte(base_family = "Helvetica"))
+
+
 
 sfs_syn <- read_csv("results/sfs-synonymous.csv") %>% 
   mutate(
@@ -16,90 +14,7 @@ sfs_nonsyn <- read_csv("results/sfs-nonsynonymous.csv") %>%
 
 
 sfs <- bind_rows(sfs_nonsyn, sfs_syn) %>% 
-  filter(!n %in% c(0, 100)) # Drop fixated allels
-
-
-sfs %>% 
-  filter(Population == "MXL") %>% 
-  ggplot(aes(x = n, y = Freq)) +
-  geom_line() +
-  scale_y_log10() +
-  facet_grid(.~cat)
-
-
-
-sfs %>% 
-  ggplot(aes(x = n, y = Freq, group = Population)) +
-  geom_area() +
-  scale_y_log10() +
-  facet_grid(cat ~ Population)
-
-
-
-sfs %>% 
-  filter(Population == "MXB") %>% 
-  ggplot(aes(x = n, y = Freq)) +
-  geom_bar(aes(fill = cat), stat = "identity", position = position_dodge(width = 0.7)) +
-  scale_y_log10() +
-  scale_fill_manual(values = c("#d95f02", "#7570b3"))
-
-
-
-sfs %>% 
-  ggplot(aes(x = n, y = Freq, color = cat, fill = cat)) +
-  geom_point(size = 0.5) +
-  geom_line() +
-  geom_area(position = "dodge", alpha = 0.1) +
-  scale_y_log10() +
-  facet_grid(~Population) +
-  scale_fill_manual(values = c("#d95f02", "#7570b3")) +
-  scale_color_manual(values = c("#d95f02", "#7570b3"))
-
-
-# non-syn case ------------------------------------------------------------
-
-p_nsyn <- sfs %>% 
-  filter(cat == "Non-synonymous") %>% 
-  ggplot(aes(x = n, y = Freq, color = Population)) +
-  geom_line(color= "black") +
-  geom_rangeframe(color = "black", size = 0.5) +
-  gghighlight(use_direct_label = FALSE)  +
-  scale_y_log10() +
-  facet_wrap(~Population) +
-  labs(
-    title = "Non-synonymous Variants"
-  )
-
-p_syn <- sfs %>% 
-  filter(cat == "Synonymous") %>% 
-  ggplot(aes(x = n, y = Freq, color = Population)) +
-  geom_line(color= "black") +
-  geom_rangeframe(color = "black", size = 0.5) +
-  gghighlight(use_direct_label = FALSE)  +
-  scale_y_log10() +
-  facet_wrap(~Population) +
-  labs(
-    title = "Synonymous Variants"
-  )
-
-grid.arrange(p_syn, p_nsyn, nrow = 1)
-
-
-# make another plot -------------------------------------------------------
-
-sfs_l <- 
-  sfs %>% 
-  pivot_wider(names_from = cat, values_from = Freq) %>% 
-  mutate(
-    foldChange = log10(`Non-synonymous` / Synonymous)
-  )
-
-sfs_l %>% 
-  #filter(Population == "CHB") %>% 
-  ggplot(aes(x = n, y = foldChange)) +
-  geom_bar(stat = "identity", width = 1) +
-  facet_wrap(~Population)
-  
+  filter(!n %in% c(min(.$n), max(.$n))) # Drop fixated allels
 
 # focus on important pops -------------------------------------------------
 
@@ -115,8 +30,84 @@ sfs %>%
   ) %>% 
   ggplot(aes(x = n, y = Freq, fill=Population)) +
   geom_bar(stat = "identity", position = "dodge") +
-  #scale_fill_brewer(type = "qual", palette = 2) +
-  scale_fill_viridis_d(option = "C") +
+  scale_y_sqrt() +
+  scale_fill_viridis_d(option = "D") +
   facet_grid(cat ~ .) +
   theme_bw()
+
+ggsave("plots/sfs.pdf", height = 4, width = 9)
+ggsave("plots/sfs.png", height = 4, width = 9)
+
+# construc the plot in Henna et al 2015 -----------------------------------
+
+maf_low <- sfs %>% 
+  filter(n / max(n) < 0.18) %>% 
+  mutate(
+    MAF = "MAF < 0.18"
+  )
+
+maf_high <- sfs %>% 
+  filter(n / max(n) > 0.82) %>% 
+  mutate(
+    MAF = "MAF > 0.82"
+  )
+
+maf <- bind_rows(maf_low, maf_high) %>% 
+  filter(
+    Population %in% pops
+  ) %>%
+  mutate(
+    Population = factor(Population, levels = pops),
+    id = paste0(cat, "\n", MAF)
+  )
+
+maf %>% 
+  ggplot(aes(x = n, y = Freq, fill=Population)) +
+  geom_bar(stat = "identity", position = "dodge", alpha = 0.85) +
+  scale_y_sqrt() +
+  facet_wrap(~id, scales = "free") +
+  scale_fill_viridis_d(option = "D") +
+  labs(
+    y = "Number of SNPs",
+    x = "Derived allele frequency"
+  ) +
+  theme_minimal() +
+  theme(
+    legend.position = "bottom"
+  )
+
+ggsave("plots/sfs-maf.pdf", height = 4, width = 9)
+ggsave("plots/sfs-maf.png", height = 4, width = 9)
+
+
+# Syn VS non-Syn ----------------------------------------------------------
+
+sfs_dNdS <- 
+  sfs %>% 
+  pivot_wider(names_from = cat, values_from = Freq) %>% 
+  mutate(
+    logdNdS = log10(`Non-synonymous` / Synonymous)
+  ) %>% 
+  filter(
+    Population %in% pops
+  ) %>%
+  mutate(
+    Population = factor(Population, levels = pops)
+  )
+
+
+sfs_dNdS %>% 
+  ggplot(aes(x = n, y = logdNdS)) +
+  geom_bar(stat = "identity") +
+  facet_grid(Population ~.) +
+  theme_minimal() +
+  labs(
+    y = "log10 Non-Syn/Syn",
+    x = "Derived allele frequency"
+  )
+
+
+ggsave("plots/NtoNS.pdf", height = 4, width = 6)
+ggsave("plots/NtoNS.png", height = 4, width = 6)
+
 
