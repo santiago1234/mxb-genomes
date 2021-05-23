42afca22edc23557f5792c88d23af583f4de2f37
Santiago Medina <santiagogmm@hotmail.com>
2021-05-22 21:14:33 -0500

    my drawing code had a bug when drawing the tracts, it was generating overlapping tracts

--- analysis-doc/210322-LocalAncestryVisuzlization/scripts/karyotype-local-ancestry-plot.R
+++ analysis-doc/210322-LocalAncestryVisuzlization/scripts/karyotype-local-ancestry-plot.R
@@ -6,7 +6,7 @@ theme_set(theme_tufte(base_family = "Helvetica"))
 
 # define supplied parameters ----------------------------------------------
 
-# input_bed <- "data/bed/allchrn-HG01893.tsv"
+# input_bed <- "data/3-pops/bed/allchrn-HG01893.tsv"
 # output_plot <- "plots/karyo-HG01893.png"
 # bed <- read_tsv(input_bed)
 
@@ -21,15 +21,16 @@ bed <- read_tsv(input_bed)
 
 # aling chromosomes so the 1st position is zero ---------------------------
 
-bed <- 
-  bed %>% 
-  group_by(chrn) %>% 
-  mutate(
-    spos = spos - min(spos),
-    epos = epos - min(spos)
-  ) %>% 
-  ungroup() %>% 
-  select(-c(sgpos, egpos))
+# This creates a bug when drawing the tracts
+# bed <- 
+#   bed %>% 
+#   group_by(chrn) %>% 
+#   mutate(
+#     spos = spos - min(spos),
+#     epos = epos - min(spos)
+#   ) %>% 
+#   ungroup() %>% 
+#   select(-c(sgpos, egpos))
 
 # draw a indicator variable for Hapl  otypes limits -------------------------
 
@@ -88,3 +89,34 @@ ggsave(output_plot, height = 5, width = 4)
 ggsave(
   str_replace(output_plot, ".png", ".pdf"),
   height = 5, width = 4)
+
+
+
+# code to plot start position points --------------------------------------
+
+# bed$y <- if_else(bed$ymin_haplo == 0.5, true = 0, false = 1)
+# 
+# bed %>% 
+#   ggplot(aes(x = spos, color = Ancestry)) +
+#   geom_point(aes(y = y), position = "jitter", size = 0.3) +
+#   geom_hline(yintercept = 0.5, color ="white", size = 0.1) +
+#   facet_grid(chrn ~.,switch = "y") +
+#   scale_x_continuous(
+#     expand = c(0,0),
+#     labels = label_number(scale = 1e-6, suffix = "Mb")
+#   ) +
+#   theme(
+#     axis.ticks.y = element_blank(),
+#     axis.text.y = element_blank(),
+#     strip.text.y = element_text(angle = 0),
+#     panel.background = element_blank(),
+#     legend.position = c(.8, .2)
+#   ) +
+#   labs(
+#     y = "Chromosome"
+#   ) +
+#   scale_color_viridis_d(option = "C") +
+#   labs(
+#     x = "Position relative to chromosome start",
+#     title = individual
+#   )
