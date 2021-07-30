af1bd3a96ec90f59bf2a23d5f60cddea9990a035
santiago1234 <santiagogmm@hotmail.com>
2021-07-30 12:40:44 -0500

    inference

--- demographic/models/210729-ooa/inference.py
+++ demographic/models/210729-ooa/inference.py
@@ -7,10 +7,11 @@ options = "ooa_options.yaml"
 data = "data/spectrum.pkl"
 filehandler = open(data, 'rb')
 data = pickle.load(filehandler)
+data = data.project([40, 40, 40])
 
 output = "./data/msl_best_fit_model.yml"
 ret = moments.Demes.Inference.optimize(
-    deme_graph, options, data, output=output, overwrite=True)
+    deme_graph, options, data, output=output,verbose=1, overwrite=True)
 
 
 
