6f534fbfd027ea72398b5825486087e396eab20f
Santiago Medina <santiagogmm@hotmail.com>
2021-09-24 14:12:03 -0500

    ancestral allel stat summary text

--- scripts/jsfs.py
+++ scripts/jsfs.py
@@ -78,21 +78,6 @@ def count_allels(vcf, poplabs):
     return ga.count_alleles_subpops(subpops, max_allele=1)
 
 
-def initialize_jsfs(poplabs):
-    """
-    Initialize the joint SFS
-    """
-    haploid_size = poplabs.groupby('Population').Population.count().to_dict()
-    ndim = len(haploid_size.keys())
-
-    pops_to_index = {k: i for (i, k) in enumerate(haploid_size.keys())}
-    index_to_pops = {i: k for (i, k) in enumerate(haploid_size.keys())}
-
-    dimesion = [2 * haploid_size[index_to_pops[x]] for x in range(ndim)]
-    sfs = np.zeros(dimesion, dtype=np.int32)
-    return sfs, index_to_pops, pops_to_index
-
-
 def annotate_ancestral_allel(vcf, ancgenome):
     """
     annotate the ancestral allele
@@ -101,7 +86,8 @@ def annotate_ancestral_allel(vcf, ancgenome):
                      for chrn in ancgenome.keys()}
 
     def get_anc_allele(chrn, position):
-        return ancgenome_str[chrn][position]
+        # subtract -1 to match position in sequence
+        return ancgenome_str[chrn][position - 1]
 
     aa = [get_anc_allele(c, p) for (c, p) in zip(
         vcf['variants/CHROM'], vcf['variants/POS'])]
@@ -115,9 +101,9 @@ def annotate_ancestral_allel(vcf, ancgenome):
     return pd.DataFrame(d)
 
 
-def ancestral_allel_stats(aa_table):
+def ancestral_allel_category(aa_table):
     """
-    statistics for the ancestral allel
+    Generate categories for the ancestral allele
     Returns:
         dict with information
     """
@@ -130,8 +116,8 @@ def ancestral_allel_stats(aa_table):
     stats['no_coverage'] = (aa_table.AA == '.').values
     stats['REF_is_AA'] = (aa_table.AA == aa_table.REF).values
     stats['ALT_is_AA'] = (aa_table.AA == aa_table.ALT).values
-    not_ref_alt = np.logical_or(
-        np.logical_not(stats['REF_is_AA']), 
+    not_ref_alt = np.logical_and(
+        np.logical_not(stats['REF_is_AA']),
         np.logical_not(stats['ALT_is_AA'])
     )
     stats['not_REF_or_ALT'] = np.logical_and(
@@ -139,16 +125,54 @@ def ancestral_allel_stats(aa_table):
     return stats
 
 
-vcf, poplabs, ancgenome = load_data(vcf, ancgenome, poplabs)
-aa_table = annotate_ancestral_allel(vcf, ancgenome)
+def summary_aastats(stats):
+    """
+    Returns a string summarizing the ancestral allele information
+    """
+    discarded = sum(
+        [stats[x].sum() for x in ['lowercase', 'insertion_in_extant',
+                                  'no_coverage', 'not_REF_or_ALT', 'N']]
+    )
+    kept = stats['REF_is_AA'].sum() + stats['ALT_is_AA'].sum()
+    summary = '''Total SNPs: {0}
 
-allele_counts = count_allels(vcf, poplabs)
-sfs, pops_to_index, index_to_pops = initialize_jsfs(poplabs)
 
-K = 1e3
+    SNPs discarded: {1}, {11} %
+        Why?:
+            - low confident (acgt): {2}
+            - failure (N): {3}
+            - extant species contains insertion (-): {4}
+            - no coverage in aligment (.): {5}
+            - ancestral high confidence but is not ALT or REF: {6}
+
+
+    SNPs kept (only high confidence: ACGT): {7},  {8} %
+            - REF is AA: {9}
+            - ALT is AA: {10}
+
+    '''.format(
+        stats['total'],
+        discarded,
+        stats['lowercase'].sum(),
+        stats['N'].sum(),
+        stats['insertion_in_extant'].sum(),
+        stats['no_coverage'].sum(),
+        stats['not_REF_or_ALT'].sum(),
+        kept,
+        round(100 * kept / stats['total'], 3),
+        stats['REF_is_AA'].sum(),
+        stats['ALT_is_AA'].sum(),
+        round(100 * discarded / stats['total'], 3)
+    )
+    print(summary)
+    return summary
 
 
 def locate_variant_in_sfs(allele_counts, pops_to_index, var_index):
+    """
+    Where the variant falls in the SFS?
+    Bassed on the derived allel count.
+    """
 
     var_index = int(var_index)
     n_pops = len(pops_to_index.keys())
@@ -160,10 +184,38 @@ def locate_variant_in_sfs(allele_counts, pops_to_index, var_index):
     return tuple(var_location)
 
 
-# NOTE: I need to increase the legnth of each axis by 1.
+def initialize_jsfs(poplabs):
+    """
+    Initialize the joint SFS
+    """
+    haploid_size = poplabs.groupby('Population').Population.count().to_dict()
+    ndim = len(haploid_size.keys())
+
+    def axis_length(hapsize):
+        return hapsize * 2 + 1
+
+    pops_to_index = {k: i for (i, k) in enumerate(haploid_size.keys())}
+    index_to_pops = {i: k for (i, k) in enumerate(haploid_size.keys())}
+
+    dimesion = [axis_length(haploid_size[index_to_pops[x]])
+                for x in range(ndim)]
+    sfs = np.zeros(dimesion, dtype=np.int32)
+    return sfs, index_to_pops, pops_to_index
+
+
 def populate_sfs_with_variants(sfs, allele_counts,  pops_to_index):
     n_vars = allele_counts[pops_to_index[0]].shape[0]
     for K in range(n_vars):
         var_loc = locate_variant_in_sfs(allele_counts, pops_to_index, K)
         sfs[var_loc] += 1
     return sfs
+
+
+
+
+vcf, poplabs, ancgenome = load_data(vcf, ancgenome, poplabs)
+aa_table = annotate_ancestral_allel(vcf, ancgenome)
+
+allele_counts = count_allels(vcf, poplabs)
+aa_condition = ancestral_allel_category(aa_table)
+sfs, pops_to_index, index_to_pops = initialize_jsfs(poplabs)
