16402412837fa6b923865db192fdbdf3ae7ff81f
santiago1234 <santiagogmm@hotmail.com>
2021-12-09 12:03:48 -0600

    Projected to smaller sample size to run faster #6
    I projected to a smaller sample size and reduced the number
    of iterations to 300.

---
+++ analysis-doc/210723-GeneGenealogies/readme.md
@@ -0,0 +1,1 @@
+I compressed the data directory to sake disk space. For the future i recommend to uncompressed.
--- demographic/models/211124-TestParametrizationInNative/inference.py
+++ demographic/models/211124-TestParametrizationInNative/inference.py
@@ -11,9 +11,6 @@ import pickle
 import gzip
 
 model_yml, options_params, inferred_model_yml, outfile_lkl = sys.argv[1:]
-model_yml = "ooa-4pops.yml"
-options_params = "ooa4pos_options.yaml"
-inferred_model_yml = "results/ooa-4pops-fitted.yml"
 
 # data
 sf = "../../data/210804-Compute-jSFS/data/spectrums/5d-csq-synonymous-spectrum.pkl.gz"
@@ -28,8 +25,7 @@ print("marginalizing to: ", sf.pop_ids)
 
 
 ## project and fold
-n = 35
-sf = sf.project([n] * 4)
+sf = sf.project([15, 15, 35, 15]) # MXB has the 3rd spot, we want more info there
 ## 
 sf = sf.fold()
 print(sf.shape)
@@ -43,7 +39,7 @@ ret = moments.Demes.Inference.optimize(
     inference_options=options_params,
     data=sf,
     verbose=10,
-    maxiter=1000,
+    maxiter=300,
     uL=uL,
     overwrite=True
 )
@@ -53,9 +49,9 @@ param_names, opt_params, LL = ret
 
 out_params_lkl = open(outfile_lkl, mode="w", encoding="utf-8")
 
-outfile_lkl.write('Log-likelihood: {}\n'.format(-LL))
-outfile_lkl.write("Best fit parameters:\n")
+out_params_lkl.write('Log-likelihood: {}\n'.format(-LL))
+out_params_lkl.write("Best fit parameters:\n")
 
 for n, p in zip(param_names, opt_params):
-    outfile_lkl.write(f"{n}\t{p:.3}\n")
-outfile_lkl.close()
+    out_params_lkl.write(f"{n}\t{p:.3}\n")
+out_params_lkl.close()
