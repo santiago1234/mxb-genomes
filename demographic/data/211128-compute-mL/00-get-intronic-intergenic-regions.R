f302c61d2f3ca68a0a230a0c97c6d7eacfaafcb7
Santiago Medina <santiagogmm@hotmail.com>
2022-01-11 13:46:31 -0600

    fix #19

--- demographic/data/211128-compute-mL/00-get-intronic-intergenic-regions.R
+++ demographic/data/211128-compute-mL/00-get-intronic-intergenic-regions.R
@@ -1,15 +1,14 @@
 library(GenomicRanges)
 library(magrittr)
 library(TxDb.Hsapiens.UCSC.hg38.knownGene)
-library(BSgenome.Hsapiens.UCSC.hg38.masked)
+library(BSgenome.Hsapiens.UCSC.hg38)
 library(rtracklayer)
-library(Biostrings)
 
 
 # load genomi data --------------------------------------------------------
 
 txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
-hs_genome <- BSgenome.Hsapiens.UCSC.hg38.masked
+hs_genome <- BSgenome.Hsapiens.UCSC.hg38
 
 # we want only the autosomes
 
@@ -23,14 +22,11 @@ seqlevels(txdb) <- autosomes
 intrones <- intronicParts(txdb)
 intrones <- intrones[width(intrones) > 3]
 
-
+intrones <- reduce(intrones)
+names(intrones) <- paste0('intron_', 1:length(intrones))
 export.bed(intrones, "data/regions/introns.bed")
-intronic_seqs <- getSeq(hs_genome, intrones)
-# add a fake name
-names(intronic_seqs) <- paste0("intron_", 1:length(intronic_seqs))
 
 
-writeXStringSet(intronic_seqs, "data/regions/introns.fasta", format = "fasta")
 
 # get intergenic regions for H.  sapiens ----------------------------------
 # See: Bioinformatic Data Skills, page: 319
@@ -38,12 +34,9 @@ writeXStringSet(intronic_seqs, "data/regions/introns.fasta", format = "fasta")
 chrom_grngs <- as(seqinfo(txdb), "GRanges")
 collpased_tx <- reduce(transcripts(txdb))
 strand(collpased_tx) <- "*"
+
 intergenic <- setdiff(chrom_grngs, collpased_tx)
 intergenic <- intergenic[width(intergenic) > 3]
+names(intergenic) <- paste0('intergenic_', 1:length(intergenic))
 
 export.bed(intergenic, "data/regions/intergenic.bed")
-
-intergenic_seqs <- getSeq(hs_genome, intergenic)
-names(intergenic_seqs) <- paste0("intergenic_", 1:length(intergenic_seqs))
-
-writeXStringSet(intergenic_seqs, "data/regions/intergenic.fasta", format = "fasta")
