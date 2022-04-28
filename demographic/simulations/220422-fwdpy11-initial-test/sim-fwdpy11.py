8f014c1a5ade893cc7d7e110c4ae5a1abb4b2c41
santiago1234 <santiagogmm@hotmail.com>
2022-04-28 18:24:06 -0500

    run neutral simulations #61

---
+++ demographic/simulations/220422-fwdpy11-initial-test/sim-fwdpy11.py
@@ -0,0 +1,63 @@
+import fwdpy11
+import demes
+import gzip
+import time
+
+
+# load the graph and import it as a fwdpy11 demography object
+graph = demes.load('ADMIXTURE-MXL.yml')
+demog = fwdpy11.discrete_demography.from_demes(graph)
+
+# set up the region to simulate
+L = 1e6  # length in base pairs
+r = 1e-8  # the recombination rate per base pair
+print("r * L =", r * L)
+
+
+# initialize the ancestral population
+print("Initial sizes =", demog.metadata["initial_sizes"])
+initial_sizes = [
+    demog.metadata["initial_sizes"][i]
+    for i in sorted(demog.metadata["initial_sizes"].keys())
+]
+pop = fwdpy11.DiploidPopulation(initial_sizes, L)
+
+
+# set up the random number generator
+rng = fwdpy11.GSLrng(54321)  # should take a seed in the future
+
+# the parameters that fwdpy11 needs to run the simulation
+p = {
+    "nregions": [],  # neutral mutations (none for now, can add after the fact)
+    "gvalue": fwdpy11.Additive(2.0),  # fitness model
+    "sregions": [],  # selected mutations (none for now)
+    "recregions": [fwdpy11.PoissonInterval(0, L, r * L)],  # total rec rate in region
+    "rates": (0.0, 0.0, None),  # neutral and selected mutations have rates 0
+    "prune_selected": False,
+    "demography": demog,  # pass the demographic model
+    "simlen": demog.metadata["total_simulation_length"],  # the total time to simulate
+}
+params = fwdpy11.ModelParams(**p)
+
+
+# run the simulation
+print('runnning simulation ...')
+time1 = time.time()
+fwdpy11.evolvets(
+    rng, pop, params, simplification_interval=100, suppress_table_indexing=True
+)
+print("Simulation took", int(time.time() - time1), "seconds")
+
+# simulation finished
+print("Final population sizes =", pop.deme_sizes())
+
+# add neutral mutations
+# The mutation rate, per haploid genome per generation
+u = 1e-3
+nmuts = fwdpy11.infinite_sites(rng, pop, u)
+print(f"{nmuts} neutral mutations added")
+
+# save the simulated pop
+print('saving simulated population ...')
+with gzip.open('data/sim-pop.gz', 'wb') as f:
+    pop.pickle_to_file(f)
---
+++ demographic/simulations/220422-fwdpy11-initial-test/sim-msprime.py
@@ -0,0 +1,42 @@
+import msprime
+import demes
+import tskit
+import gzip
+
+# Simulation parameters. This parameters
+# should match those in sim-fwdpy11.py
+rec_rate = 1e-8
+# the mutation rate
+u = 1e-8
+N_ind_per_deme = 50  #samples per population 
+L = 1e6
+
+
+graph = demes.load("ADMIXTURE-MXL.yml")
+
+print('setting demography')
+demography = msprime.Demography.from_demes(graph)
+
+print('setting evolution model')
+evolution_model = [msprime.DiscreteTimeWrightFisher(duration=20), msprime.StandardCoalescent()]
+
+# Samples to simulate
+demes = ['YRI', 'IBS', 'CHB', 'MXB', 'MXL']
+Samples = [msprime.SampleSet(N_ind_per_deme, population=x) for x in demes]
+
+print('running simulation ...')
+ts = msprime.sim_ancestry(
+    samples=Samples,
+    demography=demography,
+    recombination_rate=rec_rate, 
+    sequence_length=L,
+    model=evolution_model,
+    ploidy=2,
+    random_seed=42
+)
+
+print('adding mutations ...')
+# We set discrete_genome=False to use the infinite sites model
+mts = msprime.sim_mutations(ts, rate=1e-8, random_seed=42, discrete_genome=False)
+
+mts.dump('data/ts-msprime.ts')
